e8ea6c0ef61640a9a69ad47d56fd65e7fcd95f85
zheng <13221035103@163.com>
2019-06-26 22:56:28 +0800

    更新

--- BaseCommon/src/main/java/com/app/basecommon/common/Constant.java
+++ BaseCommon/src/main/java/com/app/basecommon/common/Constant.java
@@ -7,7 +7,7 @@ package com.app.basecommon.common;
  */
 
 public interface Constant {
-    String BASE_URL = "http://192.168.10.195:3000";
+    String BASE_URL = "http://192.168.124.11:3000";
     int OKHTTP_TIMEOUT = 10;
     String ACCESS_TOKEN = "ACCESSTOKEN";
 }
